8978f01eff6195ddd263e6e0cb20a1f2f6254b73
Ac16ll04ig4 <ameriecallegari@hotmail.com>
2025-02-22 10:45:07 +0100

    P2_Start_L1-1

--- startcode/a_raad_het_getal.py
+++ startcode/a_raad_het_getal.py
@@ -1,5 +1,19 @@
-# Schrijf een functie is_getal_geraden die op basis van een gok en een geheim_nummer controleert of de gok correct was.
-# Print hier een boodschap, en return de juiste waarde.
+import random
+def is_getal_geraden (gok, geheim_nummer):
+    if gok == geheim_nummer:
+        print("Correct")
+        return True
+    else:
+        print("Fout")
+        return False
 
+def raad_het_getal (bovengrens):
+    geheim_nummer = random.randint(1, bovengrens)
+    einde_spel = False
+    while not einde_spel:
+        gok = int(input("Geef een getal"))
+        geraden = is_getal_geraden(gok, geheim_nummer)
+        if geraden == True:
+            einde_spel = True
 
-# Schrijf een functie raad_het_getal, die op basis van een bovengrens het raadspel zal spelen.
+raad_het_getal(40)
--- startcode/dobbelspel/main.py
+++ startcode/dobbelspel/main.py
@@ -1,1 +1,24 @@
-# Plaats hierin de main-functie
+from startcode.tictactoe.mytictactoe import initialiseer_bord, print_bord, zet, controleer_winnaar
+
+
+def tic_tac_toe():
+    bord = initialiseer_bord()
+    huidige_speler = 'x'
+    einde_spel = False
+    winnaar = ""
+    teller = 0
+    while not einde_spel:
+        print_bord(bord)
+        rij = int(input("Doe een zet."))
+        kolom = int(input("Doe een zet in een kolom."))
+
+        bord = zet(bord, rij, kolom, huidige_speler)
+        if (controleer_winnaar(bord)== True):
+            print(f"huidige_speler) heeft gewonnen!")
+
+        if huidige_speler == 'x':
+           huidige_speler = 'o'
+        else:
+            huidige_speler = 'x'
+
+tic_tac_toe()
